26fc70dbce2c5b6150c8fc97a11a638cd0105804
Naz Malik <naz@Nazs-MacBook-Pro.local>
2017-09-21 13:58:57 +0100

    Move pusher logic into pusherRequester

--- src/app.js
+++ src/app.js
@@ -1,76 +1,31 @@
 'use strict';
 
-const pusher = new Pusher('878d874539a144210a3c', {
-  cluster: 'eu',
-  encrypted: true
-});
+document.addEventListener("DOMContentLoaded", function() {
 
-var channel = pusher.subscribe('office');
+  new PusherRequester;
 
-channel.bind('pusher:subscription_succeeded', function() {
-  console.log('Pusher subscription succeeded');
-});
-
-channel.bind('state', function(data) {
-  updateView(data);
-});
-
-function updateView(data) {
-  updateAreas(data);
-  updateLargeEmoji(data);
-};
-
-function updateAreas(data) {
-  var areas = [office, people, coffee, food, darts]
-
-  areas.forEach(function(area) {
-    area.innerHTML = emojify(data[area.id]);
+  document.addEventListener("click", function(event) {
+    clearSelected()
+    var targetElement = event.target;
+    updateTargetElement(targetElement);
+    selectLargeEmoji(targetElement);
   });
-}
 
-function updateLargeEmoji(data) {
-  var selectedEmoji = document.getElementById("selected-emoji");
-  var selectedArea = document.getElementsByClassName("selected")[0]
-  var selectedType = selectedArea.children[0].id
+  function updateTargetElement(element) {
+    if (element.classList.contains("area")) {
+      element.classList.add("selected");
+    };
+  }
 
-  selectedEmoji.innerHTML = emojify(data[selectedType]);
-}
+  function selectLargeEmoji(element) {
+    var selectedEmoji = document.getElementById("selected-emoji");
+    selectedEmoji.innerHTML = element.children[0].innerHTML
+  }
 
-function emojify(score) {
-  if (score >= 8) {
-    return '😄 ';
-  } else if (score >= 6) {
-    return '🙂 ';
-  } else if (score >= 4) {
-    return '😐 ';
-  } else if (score >= 2) {
-    return '😞 ';
-  } else if (score >= 0) {
-    return '😠 ';
+  function clearSelected() {
+    var selected = document.getElementsByClassName("selected");
+    Array.prototype.forEach.call(selected, function(element) {
+      element.classList.remove("selected");
+    });
   };
-}
-
-document.addEventListener("click", function(event) {
-  clearSelected()
-  var targetElement = event.target;
-  updateTargetElement(targetElement);
-  selectLargeEmoji(targetElement);
-});
-
-function updateTargetElement(element) {
-  if (element.classList.contains("area")) {
-    element.classList.add("selected");
-  };
-}
-
-function selectLargeEmoji(element) {
-  var selectedEmoji = document.getElementById("selected-emoji");
-  selectedEmoji.innerHTML = element.children[0].innerHTML
-}
-
-function clearSelected() {
-  var selected = document.getElementsByClassName("selected");
-  Array.prototype.forEach.call(selected, function(element) {
-    element.classList.remove("selected");
-  });
-};
+})
---
+++ src/pusherRequester.js
@@ -0,0 +1,52 @@
+'use strict';
+
+function PusherRequester() {
+  const pusher = new Pusher('878d874539a144210a3c', {
+    cluster: 'eu',
+    encrypted: true
+  });
+
+  var channel = pusher.subscribe('office');
+
+  channel.bind('pusher:subscription_succeeded', function() {
+    console.log('Pusher subscription succeeded');
+  });
+
+  channel.bind('state', function(data) {
+    updateView(data);
+  });
+
+  function updateView(data) {
+    updateAreas(data);
+    updateLargeEmoji(data);
+  };
+
+  function updateAreas(data) {
+    var areas = [office, people, coffee, food, darts]
+    areas.forEach(function(area) {
+      area.innerHTML = emojify(data[area.id]);
+    });
+  }
+
+  function updateLargeEmoji(data) {
+    var selectedEmoji = document.getElementById("selected-emoji");
+    var selectedArea = document.getElementsByClassName("selected")[0]
+    var selectedId = selectedArea.children[0].id
+
+    selectedEmoji.innerHTML = emojify(data[selectedId]);
+  }
+
+  function emojify(score) {
+    if (score >= 8) {
+      return '😄 ';
+    } else if (score >= 6) {
+      return '🙂 ';
+    } else if (score >= 4) {
+      return '😐 ';
+    } else if (score >= 2) {
+      return '😞 ';
+    } else if (score >= 0) {
+      return '😠 ';
+    };
+  }
+};
