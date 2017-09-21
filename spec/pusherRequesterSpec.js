e9a4392ce009f45ed6c49814d64c5d341b9bb832
Naz Malik <naz@Nazs-MacBook-Pro.local>
2017-09-21 15:13:04 +0100

    Test emojify function

--- spec/pusherRequesterSpec.js
+++ spec/pusherRequesterSpec.js
@@ -9,9 +9,23 @@ describe("PusherRequester", function() {
   });
 
   describe("emojify", function() {
-    it("returns a very happy emoji if score is between 8 and 10", function() {
-      console.log(requester);
+    it("very happy emoji if score is between 8 and 10", function() {
       expect(requester.emojify(8)).toEqual('😄 ');
+    });
+
+    it("happy emoji if score is between 6 and 8", function() {
+      expect(requester.emojify(7)).toEqual('🙂 ');
+    });
+
+    it("neutral emoji if score is between 4 and 6", function() {
+      expect(requester.emojify(4.5)).toEqual('😐 ');
+    });
+    it("sad emoji if score is between 2 and 4", function() {
+      expect(requester.emojify(2.25)).toEqual('😞 ');
+    });
+
+    it("angry emoji if score is between 0 and 2", function() {
+      expect(requester.emojify(0)).toEqual('😠 ');
     });
   });
 
